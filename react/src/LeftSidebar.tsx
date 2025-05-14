3e7cad7c9cb89c7d3a12a28bcbd0ffb4963d65d1
oozzy <weixuanf@outlook.com>
2025-05-15 01:48:51 +0800

    fix leftbar ui

--- react/src/LeftSidebar.tsx
+++ react/src/LeftSidebar.tsx
@@ -24,39 +24,42 @@ export default function LeftSidebar({
     fetchChatSessions();
   }, []);
   return (
-    <div className="flex flex-col gap-4 p-3">
-      <Button size={"sm"} variant={"outline"} className="w-full">
-        <PlusIcon /> New Chat
-      </Button>
-      <Button
-        size={"sm"}
-        variant={"outline"}
-        className="w-full"
-        onClick={() => {
-          alert(
-            "AI content writing Copilot is coming soon! We're working on it as fast as we can!"
-          );
-        }}
-      >
-        <PencilIcon className="w-4 h-4 text-xs size-4" /> Write
-      </Button>
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-col gap-4 p-3 sticky top-0">
+        <Button size={"sm"} variant={"outline"} className="w-full">
+          <PlusIcon /> New Chat
+        </Button>
+        <Button
+          size={"sm"}
+          variant={"outline"}
+          className="w-full"
+          onClick={() => {
+            alert(
+              "AI content writing Copilot is coming soon! We're working on it as fast as we can!"
+            );
+          }}
+        >
+          <PencilIcon className="w-4 h-4 text-xs size-4" /> Write
+        </Button>
+      </div>
 
-      <div className="flex flex-col text-left justify-start overflow-y-auto h-full">
-        {chatSessions.map((session) => (
-          <Button
-            key={session.id}
-            variant={session.id === sessionId ? "default" : "ghost"}
-            className="justify-start text-left px-2 w-full"
-            onClick={() => {
-              setSessionId(session.id);
-            }}
-          >
-            {/* <Spinner /> */}
-            <span className="truncate">
-              {!!session.title ? session.title : "Untitled"}
-            </span>
-          </Button>
-        ))}
+      <div className="flex-1 overflow-y-auto px-3">
+        <div className="flex flex-col text-left justify-start">
+          {chatSessions.map((session) => (
+            <Button
+              key={session.id}
+              variant={session.id === sessionId ? "default" : "ghost"}
+              className="justify-start text-left px-2 w-full"
+              onClick={() => {
+                setSessionId(session.id);
+              }}
+            >
+              <span className="truncate">
+                {!!session.title ? session.title : "Untitled"}
+              </span>
+            </Button>
+          ))}
+        </div>
       </div>
     </div>
   );
