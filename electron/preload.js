3e58d2edcc7e7142bbbb69a12777de6fed562996
oozzy <weixuanf@outlook.com>
2025-05-17 18:58:31 +0800

    can call ipc

--- electron/preload.js
+++ electron/preload.js
@@ -1,8 +1,25 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electronAPI", {
-  // ... existing exposed APIs ...
+// const ipcHandlers = require("./ipcHandlers");
 
+// console.log("Available IPC handlers:", Object.keys(ipcHandlers));
+
+// // Dynamically build API based on handler function names
+// const exposedAPI = {};
+// for (const name of Object.keys(ipcHandlers)) {
+//   exposedAPI[name] = (...args) => {
+//     console.log(`Calling IPC handler: ${name} with args:`, args);
+//     return ipcRenderer.invoke(name, ...args);
+//   };
+// }
+
+// console.log("Exposing API with methods:", Object.keys(exposedAPI));
+
+contextBridge.exposeInMainWorld("electronAPI", {
+  publishRednote: (...args) => {
+    console.log("🦄publishRednote called with args:", args);
+    return ipcRenderer.invoke("publishRednote", ...args);
+  },
   // Add new file picker methods
   pickImage: () => ipcRenderer.invoke("pick-image"),
   pickVideo: () => ipcRenderer.invoke("pick-video"),
