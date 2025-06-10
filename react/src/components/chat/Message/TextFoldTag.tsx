6536e95ceae555b1d4e63ba687ec3e41359bb960
Junwei Zhang <1621422515@qq.com>
2025-06-10 15:38:48 +0800

    add svg to tag

--- react/src/components/chat/Message/TextFoldTag.tsx
+++ react/src/components/chat/Message/TextFoldTag.tsx
@@ -36,7 +36,10 @@ const TextFoldTag: React.FC<TextFoldTagProps> = ({
           )}
         />
         <span className="truncate max-w-[80%] inline-block">
-          <span className="font-semibold text-muted-foreground">
+          <span className="font-semibold text-muted-foreground flex items-center gap-2">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+              <path clipRule="evenodd" fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 0 0 6 21.75a6.721 6.721 0 0 0 3.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 0 1-.814 1.686.75.75 0 0 0 .44 1.223ZM8.25 10.875a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25ZM10.875 12a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Zm4.875-1.125a1.125 1.125 0 1 0 0 2.25 1.125 1.125 0 0 0 0-2.25Z"></path>
+            </svg>
             {buttonText || t('chat:thinking.title')}
           </span>
         </span>
