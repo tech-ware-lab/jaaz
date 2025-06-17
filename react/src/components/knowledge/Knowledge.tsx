54a4e3bb55565f1fa8bd4eede5006ba699fe434d
oozzy <weixuanf@outlook.com>
2025-06-18 00:41:50 +0800

    add agent system prompt

--- react/src/components/knowledge/Knowledge.tsx
+++ react/src/components/knowledge/Knowledge.tsx
@@ -33,7 +33,7 @@ export default function Knowledge() {
             </Button>
           </DialogTrigger>
           <DialogContent className="max-w-[90vw]">
-            <Editor curPath={''} setCurPath={() => {}} />
+            <Editor />
           </DialogContent>
         </Dialog>
         <div
