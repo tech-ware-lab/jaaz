e1f4a34bb5a483780dc255c2f1dd2aced74a7046
Weixuan Fu <weixuanf@outlook.com>
2025-06-25 10:55:12 +0800

    hide add comfy workflow until it is ready to use

--- react/src/components/settings/ComfuiWorkflowSetting.tsx
+++ react/src/components/settings/ComfuiWorkflowSetting.tsx
@@ -91,7 +91,7 @@ export default function ComfuiWorkflowSetting() {
           </DialogContent>
         </Dialog>
       )}
-      <div className="flex items-center gap-2">
+      {/* <div className="flex items-center gap-2">
         <PaletteIcon className="w-5 h-5" />
         <p className="text-sm font-bold">{t('settings:comfyui.workflows')}</p>
         <Button
@@ -105,7 +105,7 @@ export default function ComfuiWorkflowSetting() {
         {showAddWorkflowDialog && (
           <AddWorkflowDialog onClose={() => setShowAddWorkflowDialog(false)} />
         )}
-      </div>
+      </div> */}
       {/* Workflows */}
       {workflows.length > 0 && (
         <div className="space-y-2">
