845cdb59dba4466dfb0a6e5759b2614ae356b3ec
Chen Xianmin <xianmin12@gmail.com>
2025-07-02 23:03:00 +0800

    refactor: video generator init

--- server/tools/vid_generators/__init__.py
+++ server/tools/vid_generators/__init__.py
@@ -1,5 +1,7 @@
 from .volces import generate_video_volces
+from .generate_video_seedance_v1 import generate_video_seedance_v1
 
 __all__ = [
     "generate_video_volces",
+    "generate_video_seedance_v1",
 ]
