054ce47cd9e979ce8d030d3f43f3e5bff1ad6a9d
oozzy <weixuanf@outlook.com>
2025-07-12 17:26:50 +0800

    unimport gemini

--- electron/main.js
+++ electron/main.js
@@ -4,7 +4,8 @@
 const fs = require('fs')
 const path = require('path')
 const os = require('os')
-const gemini = require('./dist/gemin_service')
+// to import a ts module, we need to import like below
+// const gemini = require('./dist/gemin_service')
 const logPath = path.join(os.homedir(), 'jaaz-log.txt')
 // Check if the log file exists and delete it
 if (fs.existsSync(logPath)) {
