a655500cc22eaa0f14e4617c3ff74589684a5287
oozzy <weixuanf@outlook.com>
2025-07-22 22:02:40 +0800

    add posthog

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -2,17 +2,23 @@ import { SocketProvider } from '@/contexts/socket'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-
+import { PostHogProvider } from 'posthog-js/react'
 import '@/assets/style/index.css'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <SocketProvider>
-        <App />
-      </SocketProvider>
+      <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </PostHogProvider>
     </StrictMode>
   )
 }
